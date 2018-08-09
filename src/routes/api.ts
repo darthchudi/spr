f84218a8121bd41bf1b3f78c72842ac329f2b1c8
darthchudi <chukwudioranu@ymail.com>
2018-08-09 10:21:01 +0100

    feat: create login and get superhero route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import {createSuperHero} from "../controllers/superheroController"
+import {createSuperHero, loginSuperhero, getSuperHero} from "../controllers/superheroController"
 const router = express.Router();
 
 /* GET home page. */
 router.post('/create', createSuperHero);
 
+router.post('/login', loginSuperhero);
+
+router.get('/superhero', getSuperHero);
 
 export default router;
