b06b0caf13a92395ddf4c9b5df63de9ed160b4f4
chudi <chukwudioranu@ymail.com>
2018-08-13 11:35:09 +0100

    feat: create get supehero(s) handler

--- src/controllers/superheroController.ts
+++ src/controllers/superheroController.ts
@@ -1,13 +1,22 @@
-import { Request, Response } from "express";
-import { default as Superhero, ISuperhero } from "../models/Superhero";
-import { validateSuperheroInput } from "../helpers/validator";
-import { valid } from "joi";
-import jwt from "jsonwebtoken";
+import { Request, Response } from 'express';
+import { default as Superhero, SuperheroDocument } from '../models/Superhero';
+import { validateSuperheroInput } from '../helpers/validator';
+import { valid } from 'joi';
+import jwt from 'jsonwebtoken';
+
+interface SuperheroResponse{
+  govermentName: string;
+  superheroName: string;
+  superpowers: string[];
+  trait: string;
+  city: string;
+  friends: string[];
+}
 
 export async function createSuperHero(req: Request, res: Response) {
   const {
     error: validationError,
-    value: validatedInput
+    value: validatedInput,
   } = validateSuperheroInput(req.body);
 
   if (validationError) {
@@ -16,12 +25,12 @@ export async function createSuperHero(req: Request, res: Response) {
   }
 
   const existingHero = await Superhero.findOne({
-    superheroName: validatedInput.superheroName
+    superheroName: validatedInput.superheroName,
   });
 
   if (existingHero) {
     res.status(401).json({
-      message: "Super hero name is taken already"
+      message: 'Super hero name is taken already',
     });
     return;
   }
@@ -30,7 +39,7 @@ export async function createSuperHero(req: Request, res: Response) {
   newSuperhero.save((err, superhero) => {
     if (err) {
       res.status(500).json({
-        message: "Could not create superhero"
+        message: 'Could not create superhero',
       });
       return;
     }
@@ -49,23 +58,30 @@ export function loginSuperhero(req: Request, res: Response) {
   const loginSuperhero = Superhero.findOne(
     { superheroName },
     (err, superhero): any => {
-      if (err) return res.status(500).json({ message: "An error occured" });
+      if (err) return res.status(500).json({ message: 'An error occured' });
 
       if (!superhero)
-        return res.status(404).json({ message: "Superhero does not exist" });
+        return res.status(404).json({ message: 'Superhero does not exist' });
 
       superhero.comparePassword(
         plainText,
         (err: any, isMatch: boolean): any => {
-          if (err) return res.status(500).json({ message: "An error occured" });
+          if (err) return res.status(500).json({ message: 'An error occured' });
 
           if (!isMatch)
-            return res.status(401).json({ message: "Invalid Password" });
+            return res.status(401).json({ message: 'Invalid Password' });
 
-          const token = jwt.sign({ superhero }, <string>process.env.JWT_SECRET, {expiresIn: '3h'});
+          const response = superhero.toObject();
+          delete response.password;
+
+          const token = jwt.sign(
+            { response },
+            <string>process.env.JWT_SECRET,
+            { expiresIn: '3h' }
+          );
 
           res.status(200).json({
-            token
+            token,
           });
         }
       );
@@ -73,14 +89,32 @@ export function loginSuperhero(req: Request, res: Response) {
   );
 }
 
-export function getSuperHero(req: Request, res: Response){
-    var token = req.headers.authorization;
+export function getSuperHero(req: Request, res: Response) {
+  var token = req.headers.authorization;
 
-    console.log(token);
+  jwt.verify(
+    <string>token,
+    <string>process.env.JWT_SECRET,
+    (err: any,  superhero: any): void => {
+      if (err) {
+        res
+          .status(401)
+          .json({ message: 'Token has either expired or is invalid' });
+        return;
+      }
 
-    jwt.verify(<string>token, <string>process.env.JWT_SECRET, (err, data) => {
-        if(err) return res.status(401).json({message: "Token has either expired or is invalid"});
+      res.status(200).json({ superhero });
+    }
+  );
+}
 
-        res.status(200).json({data});
-    } )
+export async function getAllSuperheros(_req: Request, res: Response){
+  try {
+    const query = Superhero.find();
+    query.select('-password');
+    const response = await query.exec();
+    return res.status(200).json({response});
+  } catch(e){
+    return res.status(500).json({error: e});
+  }
 }
