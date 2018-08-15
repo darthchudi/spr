a55bee11d4eb62734e8aa1288b24cc7b7667dee7
chudi <chukwudioranu@ymail.com>
2018-08-15 01:19:57 +0100

    chore: resolve mongodb url in staging

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,4 +1,8 @@
 export function resolveMongoDB(env? : string){
+    if(process.env.NODE_ENV === "staging"){
+        return process.env.MONGODB_URL;
+    }
+
     const envObject = !!env ? env : process.env.NODE_ENV;
 
     return envObject === "development" ? "mongodb://localhost:27017/superhero" : "REMOTE MONGO URL";
