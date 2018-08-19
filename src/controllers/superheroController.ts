cd547b790886c2aa69b9c14c429d7167a7070216
chudi <chukwudioranu@ymail.com>
2018-08-19 13:20:35 +0100

    feat: handler for sending friend requests

--- src/controllers/superheroController.ts
+++ src/controllers/superheroController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import { default as Superhero, SuperheroDocument } from '../models/Superhero';
+import {
+  default as FriendRequest,
+  FriendRequestDocument,
+} from '../models/FriendRequests';
 import { validateSuperheroInput } from '../helpers/validator';
 import { valid } from 'joi';
 import jwt from 'jsonwebtoken';
 
-interface SuperheroResponse{
+interface SuperheroResponse {
   govermentName: string;
   superheroName: string;
   superpowers: string[];
@@ -74,11 +78,9 @@ export function loginSuperhero(req: Request, res: Response) {
           const response = superhero.toObject();
           delete response.password;
 
-          const token = jwt.sign(
-            { response },
-            <string>process.env.JWT_SECRET,
-            { expiresIn: '3h' }
-          );
+          const token = jwt.sign({ response }, <string>process.env.JWT_SECRET, {
+            expiresIn: '3h',
+          });
 
           res.status(200).json({
             token,
@@ -95,7 +97,7 @@ export function getSuperHero(req: Request, res: Response) {
   jwt.verify(
     <string>token,
     <string>process.env.JWT_SECRET,
-    (err: any,  superhero: any): void => {
+    (err: any, superhero: any): void => {
       if (err) {
         res
           .status(401)
@@ -108,28 +110,71 @@ export function getSuperHero(req: Request, res: Response) {
   );
 }
 
-export async function getAllSuperheros(_req: Request, res: Response){
+export async function getAllSuperheros(_req: Request, res: Response) {
   try {
     const query = Superhero.find();
     query.select('-password');
     const response = await query.exec();
-    return res.status(200).json({response});
-  } catch(e){
-    return res.status(500).json({message: e});
+    return res.status(200).json({ response });
+  } catch (e) {
+    return res.status(500).json({ message: e });
   }
 }
 
-export async function getSuperheroFriends(req: Request, res: Response){
+export async function getSuperheroFriends(req: Request, res: Response) {
   try {
-  const id = req.params.id;
-  const superhero = await Superhero.findById(id);
-  if(superhero){
-    const {superheroName, friends} = superhero;
-    return res.status(200).json({id, superheroName, friends});
-  } else {
-    return res.status(404).json({message: "Superhero not found"})
+    const id = req.params.id;
+    const superhero = await Superhero.findById(id);
+    if (superhero) {
+      const { superheroName, friends } = superhero;
+      return res.status(200).json({ id, superheroName, friends });
+    } else {
+      return res.status(404).json({ message: 'Superhero not found' });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: e });
   }
-} catch(e){
-  return res.status(500).json({message: e});
 }
+
+export async function sendFriendRequest(req: Request, res: Response) {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      message: 'Unauthorized action!',
+    });
+  }
+
+  const authorization = <string>req.headers.authorization;
+  const [_, token] = authorization.split(' ');
+  const recieverId = req.body.reciever;
+  const {
+    response: { _id: senderId },
+  }: any = await jwt.verify(token, <string>process.env.JWT_SECRET);
+
+  Superhero.findById(recieverId, (err, reciever) => {
+    if (err || !reciever)
+      return res.status(500).json({
+        message:
+          'There was an error finding the reciever of the friend request',
+      });
+
+    const newFriendRequest = new FriendRequest({
+      sender: senderId,
+      reciever: reciever._id,
+    });
+
+    newFriendRequest.save((err, _sentRequest) => {
+      if (err)
+        return res.status(500).json({
+          message: 'There was an error sending the friend request',
+        });
+
+      return res.status(200).json({
+        message: `Friend request successfully sent to ${
+          reciever.superheroName
+        }`,
+      });
+    });
+    return;
+  });
+  return;
 }
