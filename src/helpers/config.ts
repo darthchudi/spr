c02b6421cf0fddc54962e182ff3ba6bb7804095a
chudi <chukwudioranu@ymail.com>
2018-08-19 13:20:52 +0100

    chore: update db resolver helper

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,9 +1,7 @@
-export function resolveMongoDB(env? : string){
+export function resolveMongoDB(){
     if(process.env.NODE_ENV === "staging"){
         return process.env.MONGODB_URL;
     }
 
-    const envObject = !!env ? env : process.env.NODE_ENV;
-
-    return envObject === "development" ? "mongodb://localhost:27017/superhero" : "REMOTE MONGO URL";
+    return "mongodb://localhost:27017/superhero"
 }
